22343031d0d4c0428de525daa1e2d045f10d435f
AyushDeuja <ayushdeuja11@gmail.com>
2026-02-05 18:43:11 +0545

    added css and created basic searchlist

--- classroom-management-frontend/src/pages/subjects/list.tsx
+++ classroom-management-frontend/src/pages/subjects/list.tsx
@@ -1,5 +1,53 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Search } from "lucide-react";
+import { useState } from "react";
+
 const SubjectsList = () => {
-  return <div>SubjectsList</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedDepartment, setSelectedDepartment] = useState("all");
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subjects</h1>
+      <div className="intro-row">
+        <p>Quick Access to essential metrics and management tools.</p>
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className="search-icon" />
+            <Input
+              type="text"
+              placeholder="Search By Name.."
+              className="pl-10 w-full"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+          <div className="flex gap-2 w-full sm:w-auto">
+            <Select
+              value={selectedDepartment}
+              onValueChange={setSelectedDepartment}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Filter By Departments" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All Departments</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+    </ListView>
+  );
 };
 
 export default SubjectsList;
