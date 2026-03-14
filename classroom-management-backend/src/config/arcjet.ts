4e25dcbd214859e2d4df21b0d560ca6adccabe88
AyushDeuja <ayushdeuja11@gmail.com>
2026-03-14 17:15:11 +0545

    arcjet initiation done

---
+++ classroom-management-backend/src/config/arcjet.ts
@@ -0,0 +1,24 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
+
+if (!process.env.ARCJET_KEY && process.env.NODE_ENV !== "test") {
+  throw new Error("ARCJET_KEY is not set in .env file");
+}
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW"],
+    }),
+    // Rate Limiting
+    slidingWindow({
+      mode: "LIVE",
+      interval: "2s",
+      max: 5,
+    }),
+  ],
+});
